9604758c896dc3b7bc67278dd1c251eb04a1a5a0
hentablihamza <hentabli_hamza@yahoo.fr>
2026-03-14 12:33:53 -0500

    Add files via upload

--- ML_methods/OnionNet-2/run_onionnet2_loop.sh
+++ ML_methods/OnionNet-2/run_onionnet2_loop.sh
@@ -1,11 +1,41 @@
 #!/bin/bash
+# =========================================================
+# OnionNet-2 Batch Prediction Script
+#
+# Developed for benchmarking machine learning scoring
+# functions in virtual screening.
+#
+# Study:
+# The Last Mile Problem: A Critical Assessment of
+# Physics-based and AI Tools in Virtual Screening
+#
+# Lab:
+# Kireev Lab
+# University of Missouri-Columbia
+#
+# Author: Hamza Hentabli
+#
+# Method:
+# OnionNet-2: CNN-based model for protein–ligand
+# binding affinity prediction
+# https://github.com/zchwang/OnionNet-2
+#
+# Reference:
+# Wang Z., Zheng L., Liu Y., Qu Y., Li Y., Zhao M.,
+# Mu Y., Li W.
+# OnionNet-2: A Convolutional Neural Network Model
+# for Predicting Protein–Ligand Binding Affinity
+# Based on Residue–Atom Contacting Shells.
+# Frontiers in Chemistry, 2021.
+#
+# =========================================================
 
 # ==========================================
 # OnionNet-2 batch prediction script
 # ==========================================
 
 # Base dataset path
-DATASET_PATH="PDB"
+DATASET_PATH="PDBBind/"
 
 # index file
 INDEX_FILE="$DATASET_PATH/index.csv"
